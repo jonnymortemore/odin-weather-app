3a0952df856a895a1c5bea3ece1cb6927f45855a
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-12-31 10:37:33 +1100

    Update to class controller
    Async function usage for easier await across
    multiple functions

--- src/index.js
+++ src/index.js
@@ -9,22 +9,50 @@ import "./styles.css" // all css files need to be imported into js
   &contentType=json
 */
 
-const API_KEY = "7SNA3APJGQ2MGH23XEHQABXQ3"
-const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-const UNIT_GROUP = "metric"
-const INCLUDE = "current"
 
-function getWeatherData(location) {
-    const fetchURL = `${URL}${location}?unitGroup=${UNIT_GROUP}&include=${INCLUDE}&key=${API_KEY}&contentType=json`
+class WeatherAppController {
 
-    fetch(fetchURL)
-    .then(response => {
-	    return response.json();
-    })
-    .then(data => {
-        return console.log(data)
-    })
-    .catch(error => console.log(error));
+    API_KEY = "7SNA3APJGQ2MGH23XEHQABXQ3"
+    URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+    UNIT_GROUP = "metric"
+    INCLUDE = "current"
+
+    constructor(location) {
+        this.processNewRequest(location)
+    }
+
+    async processNewRequest(location) {
+        this.updateWeatherLocation(location)
+        this.weatherJson = await this.fetchWeatherJson()
+        this.displayWeatherData()
+    }
+
+    async fetchWeatherJson() {
+        const fetchURL = `${this.URL}${this.location}?unitGroup=${this.UNIT_GROUP}&include=${this.INCLUDE}&key=${this.API_KEY}&contentType=json`
+
+        const response = await fetch(fetchURL)
+        const data = await response.json()
+        return data
+    }
+
+    displayWeatherData() {
+        document.querySelector(".weather").innerText = this.weatherJson.currentConditions.temp;
+    }
+
+    updateWeatherLocation(newLocation) {
+        this.location = newLocation
+    }
+
+    set location(value) {
+        this._location = value
+    }
+
+    get location() {
+        return this._location
+    }
+
 }
 
-console.log(getWeatherData("Melbourne"));
+
+
+console.log(new WeatherAppController("Melbourne"));
